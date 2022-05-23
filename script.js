cea355b65690c4fd1b457f46e3961a291ac7662d
Silvio Diniz de Medeiros <silviodiniz@uol.com.br>
2022-05-23 19:37:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(inputTexto.value)
+    mensagem.value = textoEncriptado
+    mensagem.style.backgroundImage="none"
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(inputTexto.value)
+    mensagem.value = textoDesencriptado
+    mensagem.style.backgroundImage="none"
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["a", "ai"],["e", "enter"],["i", "imes"],["o", "ober"],["u", "ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+     for (var i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+     return stringEncriptada;
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["ai", "a"],["enter", "e"],["imes", "i"],["ober", "o"],["ufat", "u"]]
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+     for (var i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][0])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+     return stringDesencriptada;
+}
